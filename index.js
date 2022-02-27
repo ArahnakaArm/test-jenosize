5df564f956d3dc9272c32e1a09709e56b1348266
Teetawat <teetawatriya@gmail.com>
2022-02-27 14:13:06 +0700

    fix config4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const port = typeof process.env.PORT === 'string' ? JSON.parse(process.env.PORT) : process.env.PORT;
 const app = require('./src/app');
 const cors = require('cors');
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 // eslint-disable-next-line node/no-path-concat
 app.set('views', __dirname + '/src/views');
@@ -8,5 +9,5 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 console.log(`===============================================`);
-console.log(`Server Start PORT : ${port}`);
-app.listen(process.env.PORT || 3000);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
